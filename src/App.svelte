5aad5faaf0f1992319626deceafad4c6354a5093
Caio Cesar <caiolopesbarros@hotmail.com>
2022-06-24 15:44:10 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import {setContext} from "svelte";
+  //import {DonationService} from "./services/donation-service";
+  import Main from "./pages/Main.svelte";
+  import Router from "svelte-spa-router";
+
+  //setContext("DonationService", new DonationService("http://localhost:4000"));
+
+  let routes = {
+    "/": Main,
+  }
+</script>
+
+<div class="container">
+  <Router {routes}/>
+</div>
